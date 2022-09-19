78e09c41730a373f40db57cadf2ffd8652797cf1
Lucas Eleutério Bueno <leleuteiobueno@gmail.com>
2022-09-19 12:16:43 -0300

    Adicionando compatibilidade com PWA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './global.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +10,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
